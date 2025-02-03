891263c38f15e8a37195ae363768459a2bfcbd1d
Akshat Kharkwal <akshatkharkwal2001@gmail.com>
2025-02-03 12:18:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_bgS5AaX7X', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '65t4rfmovgniarbie5go06u7cp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'ipj228bph2bnaafsoaoo5uetb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
